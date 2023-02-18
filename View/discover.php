68967804930db7e9ce4f6f5c9297cea4f92435cf
Go6lin <zhidkivmi@yandex.ru>
2023-02-18 12:52:13 +0300

    Визуально  готово

--- View/discover.php
+++ View/discover.php
@@ -118,6 +118,290 @@
                 </div>
             </div>
         </div>
+        <div class="hot-bid-second">
+            <div class="hot-bid-second-img">
+                <img class="hot-bid-second-img" src="images/fuu-j-Lo7venJ_ywM-unsplash%201.jpg" height="290px">
+            </div>
+            <div class="hot-bid-second-main-info">
+                <div class="hot-bid-second-name-price">
+                    <div class="hot-bid-second-name">
+                        <p>Amazing digital art</p>
+                    </div>
+                    <div class="hot-bid-second-price price-tag">
+                        <p>2.45 ETH</p>
+                    </div>
+                </div>
+                <div class="hot-bid-second-follovers">
+                    <div class="hot-bid-second-follovers-photos">
+                        <img class="hot-bid-second-follovers-one circleBlock"
+                             src="images/austin-wade-X6Uj51n5CE8-unsplash%201.png" height="20">
+                        <img class="hot-bid-second-follovers-two circleBlock"
+                             src="images/austin-wade-X6Uj51n5CE8-unsplash%201.png" height="20">
+                        <img class="hot-bid-second-follovers-three circleBlock"
+                             src="images/austin-wade-X6Uj51n5CE8-unsplash%201.png" height="20">
+                    </div>
+                    <div class="hot-bid-second-follovers-amount">
+                        <p>3 in stock</p>
+                    </div>
+                </div>
+            </div>
+            <div class="hot-bid-second-sub-info">
+                <div class="hot-bid-second-settings">
+                    <img src="images/settings.png" height="20">
+                </div>
+                <div class="hot-bid-second-bid">
+                    <p class="hot-bid-second-highest-bid">Highest bid</p>
+                    <p class="hot-bid-second-bid-ETH">0.001 ETH</p>
+                </div>
+                <div class="hot-bid-second-status">
+                    <p>New bid 🔥</p>
+                </div>
+            </div>
+        </div>
+        <div class="hot-bid-third">
+            <div class="hot-bid-third-img">
+                <img class="hot-bid-third-img" src="images/fuu-j-Lo7venJ_ywM-unsplash%201.jpg" height="290px">
+            </div>
+            <div class="hot-bid-third-main-info">
+                <div class="hot-bid-third-name-price">
+                    <div class="hot-bid-third-name">
+                        <p>Amazing digital art</p>
+                    </div>
+                    <div class="hot-bid-third-price price-tag">
+                        <p>2.45 ETH</p>
+                    </div>
+                </div>
+                <div class="hot-bid-third-follovers">
+                    <div class="hot-bid-third-follovers-photos">
+                        <img class="hot-bid-third-follovers-one circleBlock"
+                             src="images/austin-wade-X6Uj51n5CE8-unsplash%201.png" height="20">
+                        <img class="hot-bid-third-follovers-two circleBlock"
+                             src="images/austin-wade-X6Uj51n5CE8-unsplash%201.png" height="20">
+                        <img class="hot-bid-third-follovers-three circleBlock"
+                             src="images/austin-wade-X6Uj51n5CE8-unsplash%201.png" height="20">
+                    </div>
+                    <div class="hot-bid-third-follovers-amount">
+                        <p>3 in stock</p>
+                    </div>
+                </div>
+            </div>
+            <div class="hot-bid-third-sub-info">
+                <div class="hot-bid-third-settings">
+                    <img src="images/settings.png" height="20">
+                </div>
+                <div class="hot-bid-third-bid">
+                    <p class="hot-bid-third-highest-bid">Highest bid</p>
+                    <p class="hot-bid-third-bid-ETH">0.001 ETH</p>
+                </div>
+                <div class="hot-bid-third-status">
+                    <p>New bid 🔥</p>
+                </div>
+            </div>
+        </div>
+        <div class="hot-bid-forth">
+            <div class="hot-bid-forth-img">
+                <img class="hot-bid-forth-img" src="images/fuu-j-Lo7venJ_ywM-unsplash%201.jpg" height="290px">
+            </div>
+            <div class="hot-bid-forth-main-info">
+                <div class="hot-bid-forth-name-price">
+                    <div class="hot-bid-forth-name">
+                        <p>Amazing digital art</p>
+                    </div>
+                    <div class="hot-bid-forth-price price-tag">
+                        <p>2.45 ETH</p>
+                    </div>
+                </div>
+                <div class="hot-bid-forth-follovers">
+                    <div class="hot-bid-forth-follovers-photos">
+                        <img class="hot-bid-forth-follovers-one circleBlock"
+                             src="images/austin-wade-X6Uj51n5CE8-unsplash%201.png" height="20">
+                        <img class="hot-bid-forth-follovers-two circleBlock"
+                             src="images/austin-wade-X6Uj51n5CE8-unsplash%201.png" height="20">
+                        <img class="hot-bid-forth-follovers-three circleBlock"
+                             src="images/austin-wade-X6Uj51n5CE8-unsplash%201.png" height="20">
+                    </div>
+                    <div class="hot-bid-forth-follovers-amount">
+                        <p>3 in stock</p>
+                    </div>
+                </div>
+            </div>
+            <div class="hot-bid-forth-sub-info">
+                <div class="hot-bid-forth-settings">
+                    <img src="images/settings.png" height="20">
+                </div>
+                <div class="hot-bid-forth-bid">
+                    <p class="hot-bid-forth-highest-bid">Highest bid</p>
+                    <p class="hot-bid-forth-bid-ETH">0.001 ETH</p>
+                </div>
+                <div class="hot-bid-forth-status">
+                    <p>New bid 🔥</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="discover-content">
+    <div class="discover-offer">
+        <div class="hot-bid-first">
+            <div class="hot-bid-first-img">
+                <img class="hot-bid-first-img" src="images/fuu-j-Lo7venJ_ywM-unsplash%201.jpg" height="290px">
+            </div>
+            <div class="hot-bid-first-main-info">
+                <div class="hot-bid-first-name-price">
+                    <div class="hot-bid-first-name">
+                        <p>Amazing digital art</p>
+                    </div>
+                    <div class="hot-bid-first-price price-tag">
+                        <p>2.45 ETH</p>
+                    </div>
+                </div>
+                <div class="hot-bid-first-follovers">
+                    <div class="hot-bid-first-follovers-photos">
+                        <img class="hot-bid-first-follovers-one circleBlock"
+                             src="images/austin-wade-X6Uj51n5CE8-unsplash%201.png" height="20">
+                        <img class="hot-bid-first-follovers-two circleBlock"
+                             src="images/austin-wade-X6Uj51n5CE8-unsplash%201.png" height="20">
+                        <img class="hot-bid-first-follovers-three circleBlock"
+                             src="images/austin-wade-X6Uj51n5CE8-unsplash%201.png" height="20">
+                    </div>
+                    <div class="hot-bid-first-follovers-amount">
+                        <p>3 in stock</p>
+                    </div>
+                </div>
+            </div>
+            <div class="hot-bid-first-sub-info">
+                <div class="hot-bid-first-settings">
+                    <img src="images/settings.png" height="20">
+                </div>
+                <div class="hot-bid-first-bid">
+                    <p class="hot-bid-first-highest-bid">Highest bid</p>
+                    <p class="hot-bid-first-bid-ETH">0.001 ETH</p>
+                </div>
+                <div class="hot-bid-first-status">
+                    <p>New bid 🔥</p>
+                </div>
+            </div>
+        </div>
+        <div class="hot-bid-second">
+            <div class="hot-bid-second-img">
+                <img class="hot-bid-second-img" src="images/fuu-j-Lo7venJ_ywM-unsplash%201.jpg" height="290px">
+            </div>
+            <div class="hot-bid-second-main-info">
+                <div class="hot-bid-second-name-price">
+                    <div class="hot-bid-second-name">
+                        <p>Amazing digital art</p>
+                    </div>
+                    <div class="hot-bid-second-price price-tag">
+                        <p>2.45 ETH</p>
+                    </div>
+                </div>
+                <div class="hot-bid-second-follovers">
+                    <div class="hot-bid-second-follovers-photos">
+                        <img class="hot-bid-second-follovers-one circleBlock"
+                             src="images/austin-wade-X6Uj51n5CE8-unsplash%201.png" height="20">
+                        <img class="hot-bid-second-follovers-two circleBlock"
+                             src="images/austin-wade-X6Uj51n5CE8-unsplash%201.png" height="20">
+                        <img class="hot-bid-second-follovers-three circleBlock"
+                             src="images/austin-wade-X6Uj51n5CE8-unsplash%201.png" height="20">
+                    </div>
+                    <div class="hot-bid-second-follovers-amount">
+                        <p>3 in stock</p>
+                    </div>
+                </div>
+            </div>
+            <div class="hot-bid-second-sub-info">
+                <div class="hot-bid-second-settings">
+                    <img src="images/settings.png" height="20">
+                </div>
+                <div class="hot-bid-second-bid">
+                    <p class="hot-bid-second-highest-bid">Highest bid</p>
+                    <p class="hot-bid-second-bid-ETH">0.001 ETH</p>
+                </div>
+                <div class="hot-bid-second-status">
+                    <p>New bid 🔥</p>
+                </div>
+            </div>
+        </div>
+        <div class="hot-bid-third">
+            <div class="hot-bid-third-img">
+                <img class="hot-bid-third-img" src="images/fuu-j-Lo7venJ_ywM-unsplash%201.jpg" height="290px">
+            </div>
+            <div class="hot-bid-third-main-info">
+                <div class="hot-bid-third-name-price">
+                    <div class="hot-bid-third-name">
+                        <p>Amazing digital art</p>
+                    </div>
+                    <div class="hot-bid-third-price price-tag">
+                        <p>2.45 ETH</p>
+                    </div>
+                </div>
+                <div class="hot-bid-third-follovers">
+                    <div class="hot-bid-third-follovers-photos">
+                        <img class="hot-bid-third-follovers-one circleBlock"
+                             src="images/austin-wade-X6Uj51n5CE8-unsplash%201.png" height="20">
+                        <img class="hot-bid-third-follovers-two circleBlock"
+                             src="images/austin-wade-X6Uj51n5CE8-unsplash%201.png" height="20">
+                        <img class="hot-bid-third-follovers-three circleBlock"
+                             src="images/austin-wade-X6Uj51n5CE8-unsplash%201.png" height="20">
+                    </div>
+                    <div class="hot-bid-third-follovers-amount">
+                        <p>3 in stock</p>
+                    </div>
+                </div>
+            </div>
+            <div class="hot-bid-third-sub-info">
+                <div class="hot-bid-third-settings">
+                    <img src="images/settings.png" height="20">
+                </div>
+                <div class="hot-bid-third-bid">
+                    <p class="hot-bid-third-highest-bid">Highest bid</p>
+                    <p class="hot-bid-third-bid-ETH">0.001 ETH</p>
+                </div>
+                <div class="hot-bid-third-status">
+                    <p>New bid 🔥</p>
+                </div>
+            </div>
+        </div>
+        <div class="hot-bid-forth">
+            <div class="hot-bid-forth-img">
+                <img class="hot-bid-forth-img" src="images/fuu-j-Lo7venJ_ywM-unsplash%201.jpg" height="290px">
+            </div>
+            <div class="hot-bid-forth-main-info">
+                <div class="hot-bid-forth-name-price">
+                    <div class="hot-bid-forth-name">
+                        <p>Amazing digital art</p>
+                    </div>
+                    <div class="hot-bid-forth-price price-tag">
+                        <p>2.45 ETH</p>
+                    </div>
+                </div>
+                <div class="hot-bid-forth-follovers">
+                    <div class="hot-bid-forth-follovers-photos">
+                        <img class="hot-bid-forth-follovers-one circleBlock"
+                             src="images/austin-wade-X6Uj51n5CE8-unsplash%201.png" height="20">
+                        <img class="hot-bid-forth-follovers-two circleBlock"
+                             src="images/austin-wade-X6Uj51n5CE8-unsplash%201.png" height="20">
+                        <img class="hot-bid-forth-follovers-three circleBlock"
+                             src="images/austin-wade-X6Uj51n5CE8-unsplash%201.png" height="20">
+                    </div>
+                    <div class="hot-bid-forth-follovers-amount">
+                        <p>3 in stock</p>
+                    </div>
+                </div>
+            </div>
+            <div class="hot-bid-forth-sub-info">
+                <div class="hot-bid-forth-settings">
+                    <img src="images/settings.png" height="20">
+                </div>
+                <div class="hot-bid-forth-bid">
+                    <p class="hot-bid-forth-highest-bid">Highest bid</p>
+                    <p class="hot-bid-forth-bid-ETH">0.001 ETH</p>
+                </div>
+                <div class="hot-bid-forth-status">
+                    <p>New bid 🔥</p>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 <div class="discover-load-more">
